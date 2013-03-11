28b16eab176f2d37576bee7cbb3cfefb6f9de0e4
Thomas DeMeo <thomasdemeo@gmail.com>
2013-03-11 16:05:00 -0400

    Added logging of lat and long to GET method;

--- lots/application/controllers/lots.php
+++ lots/application/controllers/lots.php
@@ -5,11 +5,16 @@ class Lots extends REST_Controller {
 	public function index_get()
     {
         // Display all lots
+        error_log('GET');
+        error_log($this->get('latitude'));
+        error_log($this->get('longitude'));
+
     }
 
     public function index_post()
     {
         // Create a new lot post
+        error_log('POST');
         error_log($this->post('lat'));
     }
 
